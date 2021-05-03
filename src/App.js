551bcebb3647c78f552578e9c0836b457cb2dde3
Sergio Castro <sergiocastro@MacBook-Pro-de-Sergio.local>
2021-05-03 17:07:07 -0500

    test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 				</div>
 			</div>
 
-      <h3 className='mt-3'>Add Expense</h3>
+      <h3 className='mt-3'>Add Expense :D</h3>
 			<div className='row mt-3'>
 				<div className='col-sm'>
 					<AddExpenseForm />
