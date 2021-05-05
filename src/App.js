c45e9b9de74ae202ea99fe31da59940ebe6d3edb
Johann Rojas <jrojas@modyo.com>
2021-05-05 11:36:06 -0500

    feat: Created main state of expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
@@ -7,6 +7,13 @@ import ExpenseList from './components/ExpenseList';
 import AddExpenseForm from './components/AddExpenseForm';
 
 const App = () => {
+
+  const [expenses, setExpenses] = useState([
+    { id: 12, name: 'Compras', cost: 40 },
+		{ id: 13, name: 'Navidad', cost: 400 },
+		{ id: 14, name: 'Lavado de carro', cost: 50 },
+  ])  
+  
 	return (
     <>
       <div className='row pt-3 px-5 mb-5 sticky-top' style={{background: '#cfcfcf'}}>
@@ -29,11 +36,11 @@ const App = () => {
 
           <div className="col-7 mr-4">
             <h3 className="mb-3">Expenses</h3>
-            <ExpenseList />
+            <ExpenseList expenses={expenses} />
           </div>
           <div className="col-4 border-left">
             <h3 className="mb-3">Add Expense :D</h3>
-            <AddExpenseForm />
+            <AddExpenseForm setExpenses={setExpenses} />
           </div>
 
         </div>
