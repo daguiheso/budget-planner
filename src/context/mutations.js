40284362c521198729b314e5b47389ff2a500171
Daniel H <danielhernandez16@hotmail.com>
2021-05-10 20:57:09 -0500

    feat(mutations): create file

---
+++ src/context/mutations.js
@@ -0,0 +1,20 @@
+const AppReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
+			};
+		default:
+			return state;
+	}
+};
+
+export default AppReducer;
