b8c680611b44660b777f2bd96bb853efbe1b806d
Daniel H <danielhernandez16@hotmail.com>
2021-05-10 20:58:42 -0500

    refactor(state): add useReducer

--- src/context/state.js
+++ src/context/state.js
@@ -1,30 +1,32 @@
-import React, { useState } from 'react'
-import budgetContext from './context'
+import React, { useState, useReducer } from 'react';
+import budgetContext from './context';
+import AppReducer from './mutations';
+
+const initialState = {
+	money: {
+		budget: 2000,
+		remaining: 2000,
+		limit: 1000,
+	},
+	expenses: [
+		{ id: 12, name: 'Compras', cost: 10 },
+		{ id: 13, name: 'Navidad', cost: 200 },
+		{ id: 14, name: 'Lavado de carro', cost: 40 }
+	],
+};
 
 const BudgetState = ({ children }) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    const [expenses, setExpenses] = useState([
-        { id: 12, name: 'Compras', cost: 10 },
-        { id: 13, name: 'Navidad', cost: 200 },
-        { id: 14, name: 'Lavado de carro', cost: 40 },
-    ]) 
-
-    const [money, setMoney] = useState({
-        budget: 2000,
-        remaining: 2000,
-        limit: 1000
-    })
-
-    return (
-        <budgetContext.Provider value={{
-            expenses,
-            money,
-            setExpenses,
-            setMoney
-        }}>
-            { children }
-        </budgetContext.Provider>
-    )
+	return (
+		<budgetContext.Provider value={{
+			expenses: state.expenses,
+			money: state.money,
+			dispatch,
+		}}>
+			{ children }
+		</budgetContext.Provider>
+	)
 }
 
 export default BudgetState;
